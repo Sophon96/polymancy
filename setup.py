dd0656436a0c7e7dc59b54c315a32d2e0e6daa0c
Zeyu Yao <novodoodle@gmail.com>
2024-07-05 02:20:03 -0400

    chore: Add setup.py for project packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="polymancy",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["numpy", "scipy", "scikit-learn", "pandas"],
+    author="Zeyu Yao",
+    author_email="zeyu.yao@stonybrook.edu",
+    description="Polystyrene Film Thickness and Molecular Weight Predictor for the Garcia Research Program 2024",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/polymancy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
